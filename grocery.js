d25c8327a138cbaabb5fb280ef8c9f17aae549ed
rldail <56740747+rldail@users.noreply.github.com>
2020-06-30 16:00:54 -0500

    Updated comments

--- grocery.js
+++ grocery.js
@@ -1,19 +1,22 @@
+// Simple class to define grocery items
 var Grocery = /** @class */ (function () {
-    function Grocery(name, quantity, medium) {
+    function Grocery(name, quantity, packaging) {
         this.name = name;
         this.quantity = quantity;
-        this.medium = medium;
+        this.packaging = packaging;
     }
     return Grocery;
 }());
 function init() {
+    // Create the array of groceries
     var groceryList = [new Grocery("Ovaltine", 1, "bottle"),
         new Grocery("OJ", 1, "gallon"),
         new Grocery("Milk", 2, "gallons"),
         new Grocery("Tea", 6, "boxes"),
         new Grocery("Eggs", 6, "cartons"),
         new Grocery("Bread", 1, "loaf")];
+    // Loop through the array and add the data to the html
     groceryList.forEach(function (item) {
-        document.body.innerHTML += item.name + ": " + String(item.quantity) + " " + item.medium + "<br>";
+        document.body.innerHTML += item.name + ": " + String(item.quantity) + " " + item.packaging + "<br>";
     });
 }
--- grocery.ts
+++ grocery.ts
@@ -1,12 +1,14 @@
 
+// Simple class to define grocery items
 class Grocery {
     constructor(public name: string,
                 public quantity: number,
-                public medium: string) {
+                public packaging: string) {
     }
 }
 
 function init() {
+    // Create the array of groceries
     let groceryList = [new Grocery("Ovaltine", 1, "bottle"),
                        new Grocery("OJ", 1, "gallon"),
                        new Grocery("Milk", 2, "gallons"),
@@ -14,7 +16,8 @@ function init() {
                        new Grocery("Eggs", 6, "cartons"),
                        new Grocery("Bread", 1, "loaf")]
 
+    // Loop through the array and add the data to the html
     groceryList.forEach(function(item){
-        document.body.innerHTML += item.name + ": " + String(item.quantity) + " " + item.medium + "<br>";
+        document.body.innerHTML += item.name + ": " + String(item.quantity) + " " + item.packaging + "<br>";
     });
 }
